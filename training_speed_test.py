15a967c62c98870d6d6d6bd8e02ae1fd8179c0c5
Fariz Rahman <farizrahman4u@gmail.com>
2016-08-17 02:17:00 +0530

    Update training_speed_test.py

--- training_speed_test.py
+++ training_speed_test.py
@@ -1,19 +1,20 @@
-from huffmax import Huffmax
+from huffmax2 import Huffmax
 from keras.layers import Input, Dense
 from keras.models import Model, Sequential
 import numpy as np
 import datetime
 
 
-batch_size = 100000
+batch_size = 10000
 input_dim = 100
-nb_classes = 10000
+nb_classes = 100000
 
 
 times = {}
 
 X = np.random.random((batch_size, input_dim))
 Y_huffmax = np.random.randint(0, nb_classes, size=(batch_size, 1))
+
 Y_softmax = []
 for _ in range(batch_size):
 	oh = np.zeros(nb_classes)
@@ -28,13 +29,13 @@ softmax_model.predict(X[:1])
 
 start_time = datetime.datetime.now()
 
-softmax_model.fit(X, Y_softmax, batch_size=1000)
+softmax_model.fit(X, Y_softmax, batch_size=32)
 
 end_time = datetime.datetime.now()
 
 times['Softmax'] = end_time - start_time
 
-for mode in [0, 1]:
+for mode in [0]:
 	vector = Input((input_dim,))
 	target_class = Input((1,))
 	probability = Huffmax(nb_classes, verbose=True, mode=mode)([vector, target_class])
@@ -44,7 +45,7 @@ for mode in [0, 1]:
 
 	start_time = datetime.datetime.now()
 
-	huffmax_model.fit([X, Y_huffmax], np.ones((batch_size, 1)), batch_size=1000)
+	huffmax_model.fit([X, Y_huffmax], np.ones((batch_size, 1)), batch_size=32)
 
 	end_time = datetime.datetime.now()
 
@@ -53,4 +54,3 @@ for mode in [0, 1]:
 
 for key in times.keys():
 	print(key + ' : ' + str(times[key]))
-
